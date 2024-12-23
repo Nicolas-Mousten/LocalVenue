bcd32bf259e577e5f0e1f70b2d1e79c574ac76b1
DavidGabelgaard <89967408+DavidGabelgaard@users.noreply.github.com>
2024-12-23 20:24:12 +0100

    Introduced form validation

--- LocalVenue/Web/Models/Show.cs
+++ LocalVenue/Web/Models/Show.cs
@@ -1,12 +1,14 @@
-﻿using LocalVenue.Core.Enums;
+﻿using System.ComponentModel.DataAnnotations;
+using LocalVenue.Core.Enums;
 
 namespace LocalVenue.Web.Models;
 
 public class Show
 {
     public long Id { get; set; }
+    [Required(ErrorMessage = "Skal udfyldes")]
     public string Title { get; set; } = string.Empty;
-
+    [Required(ErrorMessage = "Skal udfyldes")]
     public string? Description { get; set; }
 
     public DateTime StartTime { get; set; }
