67bd88d196cfe6c155928086b4b98cb78898b641
Nicolas-Mousten <nicolas.mousten@gmail.com>
2025-01-11 16:59:30 +0100

    test

--- LocalVenue.Tests/TicketServiceTest.cs
+++ LocalVenue.Tests/TicketServiceTest.cs
@@ -146,7 +146,68 @@ public class TicketServiceTest
     public async Task TestTicketServiceReturnSeat()
     {
         // Arrange
-        await SeedDatabaseAsync();
+        var dbContextFactory = serviceProvider.GetRequiredService<IDbContextFactory<VenueContext>>();
+
+        Customer customer = new Customer
+        {
+            Id = "0c9cd65f-2054-4086-a569-2e50997a8be3",
+            UserName = "Nicolas",
+            NormalizedUserName = "NICOLAS",
+            Email = "nicolas.mousten@gmail.com",
+            NormalizedEmail = "NICOLAS.MOUSTEN@GMAIL.COM",
+            EmailConfirmed = false,
+            PasswordHash = "AQAAAAIAAYagAAAAEJWgg4FDKFWNh/AYIzVE/3nxRluYnwDmUfDnpc75ZUylWzJYkphFBrhqFkRAgm16YA==",
+            SecurityStamp = "S4KC54SOPWVKCI7KA6MGFCDMBS5SVWXG",
+            ConcurrencyStamp = "9ceff3cb-b5c8-4562-bcef-4a4f0d3c3761",
+            PhoneNumber = null,
+            PhoneNumberConfirmed = false,
+            TwoFactorEnabled = false,
+            LockoutEnd = null,
+            LockoutEnabled = false,
+            AccessFailedCount = 0,
+            FirstName = "Nicolas",
+            LastName = "Mousten",
+            IsGoldenMember = false,
+            Tickets = new List<Ticket>()
+        };
+        Show show = new Show
+        {
+            ShowId = 3,
+            Title = "TestShow",
+            Description = "A show to test on",
+            StartTime = DateTime.Now.AddHours(12),
+            EndTime = DateTime.Now.AddHours(14),
+            Genre = Genre.Musical,
+            OpeningNight = false,
+            Tickets = new List<Ticket>()
+        };
+        Seat seat = new Seat
+        {
+            SeatId = 3,
+            Section = "Front",
+            Row = 1,
+            Number = 1
+        };
+        Ticket ticket = new Ticket
+        {
+            TicketId = 3,
+            ShowId = show.ShowId,
+            Show = show,
+            SeatId = 3,
+            Seat = seat,
+            Price = 50,
+            Status = Status.Sold,
+            CustomerId = "0c9cd65f-2054-4086-a569-2e50997a8be9",
+            Customer = customer
+        };
+
+        show.Tickets?.Add(ticket);
+        await using (var context = await dbContextFactory.CreateDbContextAsync())
+        {
+            context.Users.Add(customer);
+            context.Shows.Add(show);
+            await context.SaveChangesAsync();
+        }
 
         // Act
         var contextFactoryRetrieve = serviceProvider.GetRequiredService<IDbContextFactory<VenueContext>>();
