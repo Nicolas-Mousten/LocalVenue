5fa5ea83ab44f15f467fe4a771e58cec026fa35b
DavidGabelgaard <89967408+DavidGabelgaard@users.noreply.github.com>
2025-01-12 21:01:18 +0100

    DiscountCalculator

---
+++ LocalVenue/Helpers/DiscountCalculator.cs
@@ -0,0 +1,19 @@
+﻿using LocalVenue.Web.Models;
+
+namespace LocalVenue.Helpers
+{
+    public static class DiscountCalculator
+    {
+        public static List<Ticket> ApplyDiscount(this List<Ticket> tickets, int ticketsAlreadyPurchased)
+        {
+            {
+                foreach (var ticket in tickets.OrderByDescending(x => x.Price).Take(ticketsAlreadyPurchased))
+                {
+                    ticket.Price *= 0.8m;
+                }
+
+                return tickets;
+            }
+        }
+    }
+}
