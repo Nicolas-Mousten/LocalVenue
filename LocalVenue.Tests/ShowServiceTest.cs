bba16dd52308a3950b6f97794ae0b2bb59f5f759
CasperRiis <7541911+CasperRiis@users.noreply.github.com>
2025-01-13 00:06:44 +0100

    Test for GetAvailableTicketsForShow

--- LocalVenue.Tests/ShowServiceTest.cs
+++ LocalVenue.Tests/ShowServiceTest.cs
@@ -29,6 +29,101 @@ public class ShowServiceTest
         serviceProvider = services.BuildServiceProvider();
     }
 
+    [Fact]
+    public async Task TestGetAvailableTicketsForShow()
+    {
+        // Arrange
+        var contextFactory = serviceProvider.GetRequiredService<IDbContextFactory<VenueContext>>();
+
+        var config = new MapperConfiguration(cfg =>
+        {
+            cfg.AddProfile<MappingProfile>();
+        });
+        var mapper = config.CreateMapper();
+
+        var ticketService = new TicketService(contextFactory, mapper);
+        var mockFactory = HttpClientFactoryHelper.GetActorServiceMockClientFactory();
+        var actorService = new ActorService(mockFactory.Object);
+
+        var service = new ShowService(contextFactory, mapper, actorService, ticketService);
+
+        var dbContextFactory = serviceProvider.GetRequiredService<
+            IDbContextFactory<VenueContext>
+        >();
+
+        await using (var context = await dbContextFactory.CreateDbContextAsync())
+        {
+            context.Seats.Add(
+                new Seat
+                {
+                    SeatId = 2,
+                    Section = "Front",
+                    Row = 1,
+                    Number = 1,
+                }
+            );
+            context.Seats.Add(
+                new Seat
+                {
+                    SeatId = 3,
+                    Section = "Front",
+                    Row = 1,
+                    Number = 2,
+                }
+            );
+            context.Seats.Add(
+                new Seat
+                {
+                    SeatId = 4,
+                    Section = "Front",
+                    Row = 1,
+                    Number = 3,
+                }
+            );
+            context.Seats.Add(
+                new Seat
+                {
+                    SeatId = 5,
+                    Section = "Front",
+                    Row = 1,
+                    Number = 4,
+                }
+            );
+            context.Seats.Add(
+                new Seat
+                {
+                    SeatId = 6,
+                    Section = "Front",
+                    Row = 1,
+                    Number = 5,
+                }
+            );
+            await context.SaveChangesAsync();
+        }
+
+        var show = new Show
+        {
+            Id = 1,
+            Title = "TestTitle",
+            Description = "TestDescription",
+            StartTime = DateTime.Now,
+            EndTime = DateTime.Now.AddHours(2),
+            Genre = Genre.Musical,
+            OpeningNight = false,
+        };
+
+        await service.CreateShowAsync(show);
+
+        // Act
+        var tickets = await service.GetAvailableTicketsForShow(1);
+
+        // Assert
+        Assert.NotNull(tickets);
+        Assert.True(tickets.Count == 5);
+
+        await serviceProvider.DisposeAsync();
+    }
+
     [Theory]
     [InlineData("TestTitle", "Title")]
     [InlineData("TestTitle", null)]
@@ -40,7 +135,7 @@ public class ShowServiceTest
 
         var config = new MapperConfiguration(cfg =>
         {
-            cfg.AddProfile<MappingProfile>(); // Assuming ShowProfile contains your mappings
+            cfg.AddProfile<MappingProfile>();
         });
         var mapper = config.CreateMapper();
 
@@ -79,7 +174,7 @@ public class ShowServiceTest
         Assert.Equal(show.Id, dataBaseShow.ShowId);
         Assert.Equal(show.Title, dataBaseShow.Title);
         Assert.Equal(show.Description, dataBaseShow.Description);
-        Assert.True(new TimeSpan(0, 0, 10) > dataBaseShow.StartTime - show.StartTime);
+        Assert.True(new TimeSpan(0, 0, 10) > dataBaseShow.StartTime - show.StartTime); // Test if the time is within 10 seconds
         // Assert.Equal(show.StartTime, dataBaseShow.StartTime);
         // Assert.Equal(show.EndTime, dataBaseShow.EndTime);
         Assert.Equal(show.Genre, dataBaseShow.Genre);
