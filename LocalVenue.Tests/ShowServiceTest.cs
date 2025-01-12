31c0a7aaeb37b77cc5c312f631a5672b8bf32f3f
CasperRiis <7541911+CasperRiis@users.noreply.github.com>
2025-01-13 00:13:49 +0100

    Test for UpdateShow

--- LocalVenue.Tests/ShowServiceTest.cs
+++ LocalVenue.Tests/ShowServiceTest.cs
@@ -22,11 +22,71 @@ public class ShowServiceTest
         services.AddDbContextFactory<VenueContext>(options =>
         {
             options
-                .UseInMemoryDatabase("InMemoryDb")
+                .UseInMemoryDatabase(databaseName: System.Guid.NewGuid().ToString())
                 .ConfigureWarnings(b => b.Ignore(InMemoryEventId.TransactionIgnoredWarning));
         });
 
         serviceProvider = services.BuildServiceProvider();
+    }
+
+    [Fact]
+    public async Task TestUpdateShow()
+    {
+        // Arrange
+        var contextFactory = serviceProvider.GetRequiredService<IDbContextFactory<VenueContext>>();
+
+        var config = new MapperConfiguration(cfg =>
+        {
+            cfg.AddProfile<MappingProfile>();
+        });
+        var mapper = config.CreateMapper();
+
+        var ticketService = new TicketService(contextFactory, mapper);
+        var mockFactory = HttpClientFactoryHelper.GetActorServiceMockClientFactory();
+        var actorService = new ActorService(mockFactory.Object);
+
+        var service = new ShowService(contextFactory, mapper, actorService, ticketService);
+
+        var show = new Show
+        {
+            Id = 1,
+            Title = "TestTitle",
+            Description = "TestDescription",
+            StartTime = DateTime.Now,
+            EndTime = DateTime.Now.AddHours(2),
+            Genre = Genre.Musical,
+            OpeningNight = false,
+        };
+
+        await service.CreateShowAsync(show);
+
+        var updatedShow = new Show
+        {
+            Id = 1,
+            Title = "UpdatedTitle",
+            Description = "UpdatedDescription",
+            StartTime = DateTime.Now.AddHours(1),
+            EndTime = DateTime.Now.AddHours(3),
+            Genre = Genre.Comedy,
+            OpeningNight = true,
+        };
+
+        // Act
+        await service.UpdateShowAsync(updatedShow);
+
+        var dataBaseShow = await service.GetShow(1);
+
+        // Assert
+        Assert.NotNull(dataBaseShow);
+        Assert.Equal(updatedShow.Id, dataBaseShow.ShowId);
+        Assert.Equal(updatedShow.Title, dataBaseShow.Title);
+        Assert.Equal(updatedShow.Description, dataBaseShow.Description);
+        Assert.Equal(updatedShow.StartTime, dataBaseShow.StartTime);
+        Assert.Equal(updatedShow.EndTime, dataBaseShow.EndTime);
+        Assert.Equal(updatedShow.Genre, dataBaseShow.Genre);
+        Assert.Equal(updatedShow.OpeningNight, dataBaseShow.OpeningNight);
+
+        await serviceProvider.DisposeAsync();
     }
 
     [Fact]
@@ -47,11 +107,7 @@ public class ShowServiceTest
 
         var service = new ShowService(contextFactory, mapper, actorService, ticketService);
 
-        var dbContextFactory = serviceProvider.GetRequiredService<
-            IDbContextFactory<VenueContext>
-        >();
-
-        await using (var context = await dbContextFactory.CreateDbContextAsync())
+        await using (var context = await contextFactory.CreateDbContextAsync())
         {
             context.Seats.Add(
                 new Seat
