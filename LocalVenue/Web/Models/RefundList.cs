b6752112802374234680870475098e3b93a2a17c
DavidGabelgaard <89967408+DavidGabelgaard@users.noreply.github.com>
2025-01-12 13:24:09 +0100

    Can now delete shows

---
+++ LocalVenue/Web/Models/RefundList.cs
@@ -0,0 +1,10 @@
+﻿namespace LocalVenue.Web.Models
+{
+    public class RefundList
+    {
+        public required int TicketCount { get; set; }
+        public required string CustomerName { get; set; }
+        public required string? CustomerEmail { get; set; }
+        public required decimal TotalAmount { get; set; }
+    }
+}
